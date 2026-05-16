f3351db4701fed1e87aa73a04b74fcbecbce4a37
ahemmm <akifm1373@gmail.com>
2026-05-17 00:29:03 +0600

    Initialize models.js with basic structure

---
+++ models.js
@@ -0,0 +1,11 @@
+// All models managed via admin panel
+const MODEL_MAP = {};
+
+const MODELS_RESPONSE = {
+  object: "list",
+  data: [],
+};
+
+const REVERSE_MAP = {};
+
+module.exports = { MODEL_MAP, MODELS_RESPONSE, REVERSE_MAP };
